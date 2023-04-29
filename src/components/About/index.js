39b64d7250db397efb2365fa7769b5d04653fd78
root <vipinrathorebdn1996@gmail.com>
2023-04-29 13:15:13 +0000

    Submit Code

---
+++ src/components/About/index.js
@@ -0,0 +1,38 @@
+// Write your code here
+import ThemeContext from '../../context/ThemeContext'
+
+import Navbar from '../Navbar'
+
+import './index.css'
+
+const About = () => (
+  <ThemeContext.Consumer>
+    {value => {
+      const {isDarkTheme} = value
+
+      const aboutBgClassName = isDarkTheme ? 'about-bg-dark' : 'about-bg-light'
+
+      const aboutImageUrl = isDarkTheme
+        ? 'https://assets.ccbp.in/frontend/react-js/about-dark-img.png'
+        : 'https://assets.ccbp.in/frontend/react-js/about-light-img.png'
+
+      const aboutTextClassName = isDarkTheme
+        ? 'about-dark-text'
+        : 'about-light-text'
+
+      return (
+        <div className={`about-app-container ${aboutBgClassName}`}>
+          <Navbar />
+          <div className="about-page-container">
+            <div className="about-container">
+              <img className="about-image" src={aboutImageUrl} alt="about" />
+              <h1 className={`about-heading ${aboutTextClassName}`}>About</h1>
+            </div>
+          </div>
+        </div>
+      )
+    }}
+  </ThemeContext.Consumer>
+)
+
+export default About
